b1dfb97639b0f7a0db69f496d945470ae53a75b3
Anderson Dalmina <andersondalmina@gmail.com>
2024-02-16 18:27:53 -0300

    fix: migration type size

--- db/migrate/20240213133517_create_rails_live_dashboard_entries.rb
+++ db/migrate/20240213133517_create_rails_live_dashboard_entries.rb
@@ -1,7 +1,7 @@
 class CreateRailsLiveDashboardEntries < ActiveRecord::Migration[7.1]
   def change
     create_table :rails_live_dashboard_entries do |t|
-      t.string :type, limit: 20, null: false
+      t.string :type, null: false
       t.string :batch_id, limit: 36, null: false
       t.json :content, null: false
       t.boolean :should_show, default: true
