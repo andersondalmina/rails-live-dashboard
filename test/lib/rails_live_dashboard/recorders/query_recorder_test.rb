fad34bb25651c4a6f4c08ae367ef13e88ae05324
Anderson Dalmina <andersondalmina@gmail.com>
2024-02-25 17:51:19 -0300

    implement tests

---
+++ test/lib/rails_live_dashboard/recorders/query_recorder_test.rb
@@ -0,0 +1,34 @@
+require 'test_helper'
+
+module RailsLiveDashboard
+  class QueryRecorderTest < ActiveSupport::TestCase
+    setup do
+      Query.destroy_all
+    end
+
+    test 'when receive a query event should create query entry' do
+      event = ActiveSupport::Notifications::Event.new(
+        'SQL Test',
+        Time.now,
+        Time.now + 1.hour,
+        '',
+        {
+          name: 'SQL Test',
+          sql: 'SELECT * FROM test WHERE ?',
+          type_casted_binds: '[test]',
+          cached: false
+        }
+      )
+
+      entry = Recorders::QueryRecorder.new(event).execute
+
+      assert_equal 1, Query.should_show.count
+      assert_equal 'SELECT * FROM test WHERE ?', entry.content.sql
+      assert_equal 'SQL Test', entry.content.name
+      assert_equal '[test]', entry.content.parameters
+      assert_equal 3_600_000, entry.content.duration.to_i
+      assert_equal 'READ', entry.content.kind
+      assert_equal false, entry.content.cached
+    end
+  end
+end
---
+++ test/lib/rails_live_dashboard/recorders/request_recorder_test.rb
@@ -0,0 +1,95 @@
+require 'test_helper'
+
+module RailsLiveDashboard
+  class RequestRecorderTest < ActiveSupport::TestCase
+    setup do
+      Request.destroy_all
+    end
+
+    test 'when receive a html request event should create request entry' do
+      event = html_event
+      entry = Recorders::RequestRecorder.new(event).execute
+
+      assert_equal 1, Request.should_show.count
+      assert_equal 'GET', entry.content.method
+      assert_equal '/test', entry.content.path
+      assert_equal 200, entry.content.status
+      assert_equal 'TestController', entry.content.controller
+      assert_equal 'index', entry.content.action
+      assert_equal 10_000, entry.content.duration.to_i
+      assert_equal '[]', entry.content.params
+      assert_equal false, entry.turbo_stream?
+    end
+
+    test 'when receive a turbo request event should create request entry' do
+      event = turbo_event
+      entry = Recorders::RequestRecorder.new(event).execute
+
+      assert_equal 1, Request.should_show.count
+      assert_equal 'GET', entry.content.method
+      assert_equal '/test', entry.content.path
+      assert_equal 200, entry.content.status
+      assert_equal 'TestController', entry.content.controller
+      assert_equal 'index', entry.content.action
+      assert_equal 10_000, entry.content.duration.to_i
+      assert_equal '[]', entry.content.params
+      assert_equal true, entry.turbo_stream?
+    end
+
+  private
+
+    def html_event
+      request_mock = ActionDispatch::Request.new({ 'REQUEST_METHOD' => 'GET' })
+      response_mock = ActionDispatch::Response.new(200, {}, [])
+
+      event = ActiveSupport::Notifications::Event.new(
+        'Test request',
+        Time.now,
+        Time.now + 10.seconds,
+        '',
+        {
+          controller: 'TestController',
+          action: 'index',
+          params: '[]',
+          format: 'html',
+          method: 'GET',
+          path: '/test',
+          status: 200,
+          headers: {},
+          body: {},
+          view_duration: 2,
+          db_duration: 3,
+          request: request_mock,
+          response: response_mock
+        }
+      )
+    end
+
+    def turbo_event
+      request_mock = ActionDispatch::Request.new({ 'REQUEST_METHOD' => 'GET' })
+      response_mock = ActionDispatch::Response.new(200, {}, [])
+
+      event = ActiveSupport::Notifications::Event.new(
+        'Test request',
+        Time.now,
+        Time.now + 10.seconds,
+        '',
+        {
+          controller: 'TestController',
+          action: 'index',
+          params: '[]',
+          format: 'turbo_stream',
+          method: 'GET',
+          path: '/test',
+          status: 200,
+          headers: {},
+          body: {},
+          view_duration: 2,
+          db_duration: 3,
+          request: request_mock,
+          response: response_mock
+        }
+      )
+    end
+  end
+end
