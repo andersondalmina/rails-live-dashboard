44ae799ae7775fe1d147a1a0444949c0516d8aa6
Ânderson Zorrer Dalmina <andersondalmina@gmail.com>
2024-02-27 23:54:02 -0300

    fix: update path with engine prefix

--- app/controllers/rails_live_dashboard/clean_controller.rb
+++ app/controllers/rails_live_dashboard/clean_controller.rb
@@ -3,7 +3,7 @@ module RailsLiveDashboard
     def destroy
       Entry.delete_all
 
-      redirect_to dashboard_path, status: :see_other
+      redirect_back fallback_location: dashboard_path, status: :see_other
     end
   end
 end
