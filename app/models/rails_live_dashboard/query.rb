d24801e2ccf1f951bb6c7e78b76b92b1b9bd8947
Ânderson Zorrer Dalmina <andersondalmina@gmail.com>
2024-02-28 01:18:35 -0300

    feat: turbo stream requests and queries

--- app/models/rails_live_dashboard/query.rb
+++ app/models/rails_live_dashboard/query.rb
@@ -1,5 +1,9 @@
 module RailsLiveDashboard
   class Query < Entry
     attribute :content, Types::QueryContent.new
+
+    after_create_commit lambda {
+      broadcast_prepend_later_to 'query_list', target: 'queries'
+    }
   end
 end
--- app/models/rails_live_dashboard/request.rb
+++ app/models/rails_live_dashboard/request.rb
@@ -2,41 +2,9 @@ module RailsLiveDashboard
   class Request < Entry
     attribute :content, Types::RequestContent.new
 
-    def method
-      content.method
-    end
-
-    def path
-      content.path
-    end
-
-    def status_code
-      content.status
-    end
-
-    def controller
-      content.controller
-    end
-
-    def controller_action
-      content.action
-    end
-
-    def duration
-      content.duration.round
-    end
-
-    def payload
-      content.params
-    end
-
-    def headers
-      content.headers
-    end
-
-    def body
-      content.body
-    end
+    after_create_commit lambda {
+      broadcast_prepend_later_to 'request_list', target: 'requests'
+    }
 
     def turbo_stream?
       content.format == 'turbo_stream'
